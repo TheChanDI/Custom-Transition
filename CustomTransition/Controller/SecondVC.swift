79ef8373c3936cd56d91912bd9e4296fcf124bbc
Prakash <chandiprakash16@gmail.com>
2022-01-12 20:00:20 +0545

    description text and bottom view added with animation

--- CustomTransition/Controller/SecondVC.swift
+++ CustomTransition/Controller/SecondVC.swift
@@ -52,6 +52,53 @@ class SecondVC: UIViewController {
         return image
     }()
 
+    lazy var descriptionLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Description\n\nThe OG Air Max Deluxe made its debut in 2000 as an answer for runners seeking a great ride and durability for miles. Today, the Air Max Deluxe is back with the same lightweight cushioning and comfort—but with modern finishes, materials and colors. Tech mesh on the vamp and a subtle, two-tone sandwich mesh on the tongue give it a fresh feel, while various shades of gray reinterpret the classic Triple Black colorway."
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .darkGray
+        label.textAlignment = .justified
+        label.numberOfLines = 0
+        label.alpha = 0
+        return label
+    }()
+    
+    lazy var bottomStackView: UIStackView = {
+       let sv = UIStackView()
+        sv.axis = .horizontal
+        sv.distribution = .fillEqually
+        
+        let v1 = UIView()
+        v1.backgroundColor = #colorLiteral(red: 0.06255983561, green: 0.1472784877, blue: 0.2344947457, alpha: 0.8271602605)
+        
+        let cartImage = UIImageView()
+        cartImage.image = UIImage(systemName: "cart")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        v1.addSubview(cartImage)
+        cartImage.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.height.width.equalTo(30)
+        }
+        
+        let v2 = UIView()
+        v2.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 0.52)
+        
+        let buyLabel = UILabel()
+        buyLabel.text = "Buy"
+        buyLabel.textColor = #colorLiteral(red: 0.06255983561, green: 0.1472784877, blue: 0.2344947457, alpha: 0.8271602605)
+        buyLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        v2.addSubview(buyLabel)
+        buyLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        sv.addArrangedSubview(v1)
+        sv.addArrangedSubview(v2)
+        
+        
+        
+        return sv
+    }()
+    
     
     //MARK: Life Cycle
     override func viewDidLoad() {
@@ -65,6 +112,9 @@ class SecondVC: UIViewController {
         configureShoeGenderLabel()
         configurePriceLabel()
         configureHeartImage()
+        configureDescriptionLabel()
+        configureBottomStackView()
+        performAnimation()
     }
     
     //MARK: - UI Setup
@@ -124,6 +174,53 @@ class SecondVC: UIViewController {
             make.bottom.equalTo(grayView.snp.top).offset(-10)
             make.height.width.equalTo(30)
         }
+    }
+    
+    private func configureDescriptionLabel() {
+        view.addSubview(descriptionLabel)
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(shoeGenderLabel.snp.bottom).offset(90)
+            make.leading.trailing.equalToSuperview().inset(20)
+       
+            
+        }
+    }
+    
+    private func configureBottomStackView() {
+        view.addSubview(bottomStackView)
+        bottomStackView.snp.makeConstraints { make in
+            make.bottom.leading.trailing.equalToSuperview()
+            make.height.equalTo(0)
+        }
+    }
+    
+    
+    //MARK: - Other Methods
+    
+    
+    private func performAnimation() {
+        self.view.layoutIfNeeded()
+        
+        //for description text
+        UIView.animate(withDuration: 1) {
+            self.descriptionLabel.alpha = 1
+            self.descriptionLabel.snp.updateConstraints { make in
+                make.top.equalTo(self.shoeGenderLabel.snp.bottom).offset(25)
+            }
+            self.view.layoutIfNeeded()
+        }
+        
+        //for bottom view
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseInOut) {
+            self.bottomStackView.snp.updateConstraints { make in
+                make.height.equalTo(70)
+            }
+            self.view.layoutIfNeeded()
+        } completion: { _ in
+            
+        }
+
+        
     }
     
     
